940f84f6bc1c07c38071cf3ccb8898d0b42283a3
joac001 <jordonez@fi.uba.ar>
2023-11-19 05:00:16 -0300

    Almmost done, no can win

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './style/App.css';
 
 function App() {
+
+  const [values, setValues] = useState([null, null, null, null, null, null, null, null, null]);
+  const [turn, setTurn] = useState(0);
+  const reset = [null, null, null, null, null, null, null, null, null];
+
+  const renderPlayed = (idx) => {
+    return values[idx] === null
+      ? <div className="empty"></div>
+      : values[idx] === 0
+        ? <div className="circle"></div>
+        : <div className="cross"><span>X</span></div>
+  }
+
+  const play = (idx) => {
+    const newValues = values.map((e, i) => i === idx ? getXorO() : values.at(i));
+    turn === 0 ? setTurn(1) : setTurn(0);
+    setValues(newValues);
+  }
+  const getXorO = () => {
+    return turn === 0 ? 0 : 1;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <div class="my-table">
+        {
+          values.map((e, idx) =>
+            <div
+              key={idx}
+
+              className={"cell idx-" + idx}
+              onClick={() => { values.at(idx) === null ? play(idx) : alert('You cant play there.'); }}
+            >
+              {renderPlayed(idx)}
+            </div>
+          )}
+      </div>
+
+      <button className='btn btn-secondary btn-restart' onClick={() => setValues(reset)} hidden={!(values.includes(0) || values.includes(1))} >
+        <span class="material-symbols-outlined">
+          replay
+        </span>
+        Restart
+      </button>
+
+    </div >
   );
 }
 
